d82dde28b9b09adecb695a7971f7e2157eb0ae60
Jesus Santiago Hernandez Lopez <ingjesussantiago@gmail.com>
2023-09-17 07:09:34 -0600

    actalizado y colocando reloj

--- src/public/js/reloj.js
+++ src/public/js/reloj.js
@@ -1,43 +1,51 @@
-let clockInterval;
-let timerRunning = false;
+document.addEventListener('DOMContentLoaded', function () {
+  const timerDisplay = document.getElementById('timer');
+  const messageDisplay = document.getElementById('message');
+  const startButton = document.getElementById('start-button');
+  const pauseButton = document.getElementById('pause-button');
 
-function startClock() {
-  if (!timerRunning) {
-    clockInterval = setInterval(updateTime, 1000);
-    timerRunning = true;
+  let minutes = 1;
+  let seconds = 0;
+  let interval;
+
+  function updateTimer() {
+    timerDisplay.innerText = `${minutes}:${seconds.toString().padStart(2, '0')}`;
   }
-}
 
-function stopClock() {
-  clearInterval(clockInterval);
-  timerRunning = false;
-}
-
-function sendBreakMessage() {
-  alert("¡Tomar un descanso de 5 minutos!");
-  setTimeout(sendResumeMessage, 300000); // 300,000 milisegundos = 5 minutos
-}
-
-function sendResumeMessage() {
-  alert("¡Reanudar actividad!");
-}
-
-function updateTime() {
-  const now = new Date();
-  const hours = now.getHours().toString().padStart(2, '0');
-  const minutes = now.getMinutes().toString().padStart(2, '0');
-  const seconds = now.getSeconds().toString().padStart(2, '0');
-  const timeString = `${hours}:${minutes}:${seconds}`;
-  document.getElementById('time').textContent = timeString;
-
-  // Verifica si han pasado 25 minutos
-  if (minutes === '25' && seconds === '00') {
-    sendBreakMessage();
+  function showMessage(message) {
+    messageDisplay.innerText = message;
   }
-}
 
-document.getElementById('startButton').addEventListener('click', startClock);
-document.getElementById('stopButton').addEventListener('click', stopClock);
+  function startPomodoro() {
+    showMessage('');
+    if (minutes === 0 && seconds === 0) {
+      minutes = 1;
+      seconds = 0;
+    }
+    interval = setInterval(() => {
+      if (minutes === 0 && seconds === 0) {
+        showMessage('¡Pomodoro terminado! Tómate un descanso.');
+        clearInterval(interval);
+        return;
+      }
+      if (seconds === 0) {
+        minutes--;
+        seconds = 59;
+      } else {
+        seconds--;
+      }
+      updateTimer();
+    }, 1000);
+  }
 
-// Iniciar el reloj automáticamente al cargar la página
-startClock();
+  function pausePomodoro() {
+    clearInterval(interval);
+    showMessage('Pomodoro pausado');
+  }
+
+  startButton.addEventListener('click', startPomodoro);
+  pauseButton.addEventListener('click', pausePomodoro);
+
+  updateTimer();
+  startPomodoro(); // Iniciar el Pomodoro automáticamente al cargar la página
+})
